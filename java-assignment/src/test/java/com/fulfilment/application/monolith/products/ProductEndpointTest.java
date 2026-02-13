dde45f2eb855b78e4368d31274652e6e7f07bc4e
Matyas <matyas.ember@gmail.com>
2026-02-13 14:02:46 +0100

    Adding add test case for product

--- java-assignment/src/test/java/com/fulfilment/application/monolith/products/ProductEndpointTest.java
+++ java-assignment/src/test/java/com/fulfilment/application/monolith/products/ProductEndpointTest.java
@@ -5,13 +5,14 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.core.IsNot.not;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class ProductEndpointTest {
 
   @Test
-  public void testCrudProduct() {
+  public void testDeleteProduct() {
     final String path = "product";
 
     // List all, should have all 3 products the database has initially:
@@ -32,5 +33,45 @@ public class ProductEndpointTest {
         .then()
         .statusCode(200)
         .body(not(containsString("TONSTAD")), containsString("KALLAX"), containsString("BESTÅ"));
+  }
+
+  @Test
+  public void testAddProduct() {
+    final String path = "product";
+
+    // List all, should have all 3 products the database has initially:
+    given()
+        .when()
+        .get(path)
+        .then()
+        .statusCode(200)
+        .body(containsString("TONSTAD"), containsString("KALLAX"), containsString("BESTÅ"));
+
+    String newProductCreationRequestMessage = """
+        {
+            "name": "BARLAST",
+            "description": null,
+            "price": null,
+            "stock": 15
+        }
+        """;
+
+    // Add the BARLAST:
+    given()
+        .contentType(ContentType.JSON)
+            .body(newProductCreationRequestMessage)
+            .when()
+        .post(path)
+        .then()
+        .statusCode(201);
+
+    // List all, TONSTAD should be missing now:
+    given()
+            .when()
+            .get(path)
+            .then()
+            .statusCode(200)
+            .body((containsString("TONSTAD")), containsString("KALLAX"),
+                    containsString("BESTÅ"), containsString("BARLAST"));
   }
 }
