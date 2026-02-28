c1cef01bd25a90198ff93f94160dba3a9189625d
Matyas <matyas.ember@gmail.com>
2026-02-28 15:22:23 +0100

    Fixing pipeline

--- java-assignment/src/test/java/com/fulfilment/application/monolith/db/FreshDbProfile.java
+++ java-assignment/src/test/java/com/fulfilment/application/monolith/db/FreshDbProfile.java
@@ -11,7 +11,7 @@ public class FreshDbProfile implements QuarkusTestProfile {
     public Map<String, String> getConfigOverrides() {
         return Map.of(
                 "quarkus.datasource.jdbc.url",
-                "jdbc:h2:mem:test-" + UUID.randomUUID(),
+                "jdbc:h2:mem:test-" + UUID.randomUUID() + ";DB_CLOSE_DELAY=-1",
                 "quarkus.hibernate-orm.database.generation",
                 "drop-and-create",
                 "quarkus.hibernate-orm.sql-load-script",
